ee2adf45d5ca5202afc5df2c3778e0e36ea0b4af
g-masdeu <guillem.masdeu97@gmail.com>
2025-12-03 20:08:18 +0100

    Corregint el 403 de verificació

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -12,7 +12,8 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        //
+        // AGREGA ESTA LÍNEA:
+        $middleware->trustProxies(at: '*');
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
